b83dc574306f99de4de3d0b77ddd0eefebb8e52a
Mohened Dev <mohenedfouad@gmail.com>
2026-04-17 17:34:53 +0300

    types.h file

---
+++ core/src/types.h
@@ -0,0 +1,60 @@
+enum class DataTypes
+{
+    INT,
+    FLOAT,
+    TEXT,
+    BOOL
+};
+
+enum class SQLKeywords
+{
+    SELECT,
+    FROM,
+    WHERE,
+    INSERT,
+    INTO,
+    VALUES,
+    UPDATE,
+    SET,
+    DELETE
+};
+
+enum class NoSQLKeywords
+{
+    FIND,
+    INSERT,
+    UPDATE,
+    DELETE
+};
+
+enum class CashKeywords
+{
+    GET,
+    SET,
+    DELETE
+};
+
+enum class LogicalOperators
+{
+    AND,
+    OR,
+    NOT
+};
+
+enum class ComparisonOperators
+{
+    EQUALS,
+    NOT_EQUALS,
+    GREATER_THAN,
+    LESS_THAN,
+    GREATER_THAN_OR_EQUALS,
+    LESS_THAN_OR_EQUALS
+};
+
+enum class ArithmeticOperators
+{
+    ADD,
+    SUBTRACT,
+    MULTIPLY,
+    DIVIDE
+};
